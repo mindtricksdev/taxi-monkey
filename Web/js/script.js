070f8ad5e282fffddc8441b8d699e4388bb453bb
mindtricksdev <daniel.iancu2012@gmail.com>
2013-07-24 15:24:52 +0300

    Added jquery ui autocomplete for Taxi costs search box

--- Web/js/script.js
+++ Web/js/script.js
@@ -1,5 +1,5 @@
 /*jslint browser:true */
-/*global $, jQuery, alert, FastClick */
+/*global $, jQuery, alert, FastClick, console */
 
 var app = {
 
@@ -12,11 +12,55 @@ var app = {
         }
     },
     
+    hideSplashScreen: function () {
+        "use strict";
+        if (navigator.splashscreen) {
+            navigator.splashscreen.hide();
+        }
+    },
+    
     initialize: function () {
         "use strict";
-        navigator.splashscreen.hide();
+        app.hideSplashScreen();
+        console.log("App initialized");
+        //app.showAlert("App initialized", "Info");
         
-        app.showAlert("App initialized", "Info");
+        // autocomplete taxi fields
+        var taxiCosts = [
+            "Taxi Alfa",
+            "Taxi Speed",
+            "Taxi Nothing"
+        ];
+        
+        $('#taxiCost').autocomplete({
+            source: function (request, response) {
+                var results = $.ui.autocomplete.filter(taxiCosts, request.term);
+                response(results.slice(0, 1));
+            },
+            appendTo: '#taxiCostsSuggestions',
+            minLength: 0,
+            create: function (event, ui) {
+                $('ul.ui-autocomplete').addClass('topcoat-list');
+                //$('#taxiCostsSuggestions').addClass('topcoat-list__container');
+                $('#taxiCostsSuggestions').hide();
+            },
+            response: function (event, ui) {
+                $('#taxiCostsSuggestions').show();
+            },
+            close: function (event, ui) {
+                $('#taxiCostsSuggestions').hide();
+            },
+            change: function (event, ui) {
+                if (!ui.item) { $(this).val(''); }
+            }
+        })
+            .data('ui-autocomplete')._renderItem = function (ul, item) {
+                return $("<li>")
+                    .data('ui-autocomplete-item', item)
+                    .append('<a>' + item.label + '</a>')
+                    .appendTo(ul)
+                    .addClass("topcoat-list__item");
+            };
     }
     
 };
@@ -37,6 +81,6 @@ $(document).ready(function () {
         onDeviceReady();
     });
     
-    
+    app.initialize();
   // Main code
 });
