81c9845b3466d00832d251978eff191edfb127a8
mindtricksdev <daniel.iancu2012@gmail.com>
2013-07-26 21:21:34 +0300

    Revert "fixed namespace"
    This reverts commit 560af7503d925c2bc0271d659acd80d7f6350b43.

--- Web/js/location.js
+++ Web/js/location.js
@@ -1,36 +1,35 @@
 /*jslint browser:true */
 /*global $, jQuery, alert, FastClick, console, MemoryAdapter, google */
 
-var geocoder;
-var latlng;
-
-var location = {
-    
-    initialize: function () {
-        "use strict";
-        geocoder = new google.maps.Geocoder();
-    },
-    
-    codeLatLng: function (lat, lng) {
-        "use strict";
-        latlng = new google.maps.LatLng(lat, lng);
-        geocoder = new google.maps.Geocoder();
-        geocoder.geocode({'latLng': latlng}, function (results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-                if (results[1]) {
-               
-                    $('#fromInfoBox').text(results[1].formatted_address);
-              
+//var geocoder;
+var locationNS = function () {
+    "use strict";
+    var geocoder, latlng;
+    var location = {
+        initialize: function () {
+            geocoder = new google.maps.Geocoder();
+        },
+        
+        codeLatLng: function (lat, lng) {
+            latlng = new google.maps.LatLng(lat, lng);
+            geocoder = new google.maps.Geocoder();
+            geocoder.geocode({'latLng': latlng}, function (results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    if (results[1]) {
+                   
+                        $('#fromInfoBox').text(results[1].formatted_address);
+                  
+                    } else {
+                        alert('No results found');
+                    }
                 } else {
-                    alert('No results found');
+                    alert('Geocoder failed due to: ' + status);
                 }
-            } else {
-                alert('Geocoder failed due to: ' + status);
-            }
-        });
-    }
+            });
+        }
+        
+      
     
-  
-
-    
+        
+    };
 };
